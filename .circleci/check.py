7f734a69f5b1f0cbf65b5d6e62348f5518025f1d
tokizuoh <tokizuoh@gmail.com>
2022-10-01 14:21:34 +0900

    add get_workflow_id_list method

--- .circleci/check.py
+++ .circleci/check.py
@@ -1,4 +1,3 @@
-from urllib import request, response
 import dotenv
 import os
 import pprint
@@ -19,10 +18,24 @@ def get_pipeline_id_list():
     except requests.exceptions.RequestException as e:
         raise e
 
+def get_workflow_id_list(pipeline_id_list):
+    url = "https://circleci.com/api/v2/pipeline/{0}/workflow".format(pipeline_id_list[0])
+    headers = {"Circle-Token": os.getenv("CIRCLE_TOKEN")}
+
+    try:
+        res = requests.get(url, headers=headers)
+        res.raise_for_status()
+        res_dict = res.json()
+        workflow_id_list = list(map(lambda item: item['id'], res_dict["items"]))
+        return workflow_id_list
+    except requests.exceptions.RequestException as e:
+        raise e
+
 def main():
     dotenv.load_dotenv()
 
     pipeline_id_list = get_pipeline_id_list()
+    workflow_id_list = get_workflow_id_list(pipeline_id_list=pipeline_id_list)
 
 if __name__ == "__main__":
     main()
